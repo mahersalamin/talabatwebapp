69556c7a90104eb48de0622ecfaac846f8c34cec
Ali Jboor <171001@ppu.edu.ps>
2020-12-03 00:16:55 +0200

    Initial Commit

---
+++ src/app/menu.ts
@@ -0,0 +1,5 @@
+export class Menu {
+    constructor(public id:number, public rest_id:number,public name:string, 
+        public descr:string, public image:string, public price:number){
+    }
+}
---
+++ src/app/restaurant.ts
@@ -0,0 +1,5 @@
+export class Restaurant {
+    constructor(public id:number, public name:string, 
+        public city:string, public image:string, public lat:number, public lng:number){
+    }
+}
